b50475bfe2d4c6a12e14d1928f1d4dea45a2048d
controladornutricionista <controlador.nutricionista.2022@gmail.com>
2022-07-18 18:54:59 -0500

    menus modificado

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,5 +1,5 @@
-const Role = require("./api/v1/database/Roles")
-const Usuario = require("./api/v1/database/Usuarios")
+const Role = require("./api/v1/database/Roles");
+const Usuario = require("./api/v1/database/Usuarios");
 
 const defaultRoles = [
   {
@@ -10,59 +10,60 @@ const defaultRoles = [
       {
         nombre: "Inicio",
         descripcion: "Inicio del aplicativo",
-        rutaPermiso: "InicioPage"
+        rutaPermiso: "InicioPage",
       },
       {
         nombre: "Login",
         descripcion: "Login del aplicativo",
-        rutaPermiso: "LoginPage"
+        rutaPermiso: "LoginPage",
       },
       {
         nombre: "ListaEmpleados",
         descripcion: "Empleados del aplicativo",
-        rutaPermiso: "EmpleadosPage"
+        rutaPermiso: "EmpleadosPage",
       },
-    ]
-  }, 
+    ],
+  },
   {
     nombre: "Empleado",
     descripcion: "Empleado del sistema",
-    color:"#6089cd",
+    color: "#6089cd",
     permisos: [
       {
         nombre: "Inicio",
         descripcion: "Inicio del aplicativo",
-        rutaPermiso: "InicioPage"
+        rutaPermiso: "InicioPage",
       },
       {
         nombre: "Login",
         descripcion: "Login del aplicativo",
-        rutaPermiso: "LoginPage"
+        rutaPermiso: "LoginPage",
       },
-    ]
+    ],
   },
   {
     nombre: "Cliente",
     descripcion: "Cliente del sistema",
-    color:"#975fcd",
+    color: "#975fcd",
     permisos: [
       {
         nombre: "Inicio",
         descripcion: "Inicio del aplicativo",
-        rutaPermiso: "InicioPage"
+        rutaPermiso: "InicioPage",
       },
       {
         nombre: "Login",
         descripcion: "Login del aplicativo",
-        rutaPermiso: "LoginPage"
-      },{
+        rutaPermiso: "LoginPage",
+      },
+      {
         nombre: "Registrar",
         descripcion: "Registrar cliente del aplicativo",
-        rutaPermiso: "RegistrarPage"
-      }
-    ]
-  }
-]
+        rutaPermiso: "RegistrarPage",
+      },
+    ],
+  },
+];
 
 const getDefaultUser = (rolId) => {
   return {
@@ -70,47 +71,50 @@ const getDefaultUser = (rolId) => {
     contrasena: "123456",
     rol: rolId,
     activo: true,
-  }
-}
-
+  };
+};
 
 exports.bootstrap = async () => {
-  const mapRoles = new Map()
+  const mapRoles = new Map();
   for (const role of defaultRoles) {
-    const existe = await Role.findOne({ "nombre": role.nombre }).exec()
-    if(!existe) {
-      const newRole = new Role(role)
+    const existe = await Role.findOne({ nombre: role.nombre }).exec();
+    if (!existe) {
+      const newRole = new Role(role);
       console.log("role created", newRole);
-      const roleSaved = await newRole.save()
-      if(!roleSaved) {
+      const roleSaved = await newRole.save();
+      if (!roleSaved) {
         console.log(`Error guardando el rol ${role.nombre}!`);
       } else {
-        mapRoles.set(role.nombre, roleSaved._id)
+        mapRoles.set(role.nombre, roleSaved._id);
       }
     } else {
-      const roleUpdated = await Role.findOneAndUpdate({ nombre: role.nombre }, {
-        $set: {
-          descripcion: role.descripcion,
-          permisos: role.permisos
-        }
-      }, { new: true }).exec()
-      mapRoles.set(role.nombre, roleUpdated._id)
+      const roleUpdated = await Role.findOneAndUpdate(
+        { nombre: role.nombre },
+        {
+          $set: {
+            descripcion: role.descripcion,
+            permisos: role.permisos,
+          },
+        },
+        { new: true }
+      ).exec();
+      mapRoles.set(role.nombre, roleUpdated._id);
     }
     console.log("hola");
   }
 
-  const user = getDefaultUser(mapRoles.get("Administrador"))
-  const adminUser = await Usuario.findOne({ usuario: user.usuario }).exec()
+  const user = getDefaultUser(mapRoles.get("Administrador"));
+  const adminUser = await Usuario.findOne({ usuario: user.usuario }).exec();
 
-  if(!adminUser) {
-    const newAdmin = new Usuario(user)
-    const userSaved = await newAdmin.save()
-    if(!userSaved) {
+  if (!adminUser) {
+    const newAdmin = new Usuario(user);
+    const userSaved = await newAdmin.save();
+    if (!userSaved) {
       console.log("El usuario por defecto no se pudo crear");
-      return
+      return;
     }
     console.log(`El usuario ${user.usuario} ha sido creado!`);
   } else {
     console.log(`El usuario ${user.usuario} ya había sido creado!`);
   }
-}
+};
